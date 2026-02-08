8f4053d36ce243db3b1bf8b9aa98f9995f3be605
MueedRaj <muhammadmueed94@gmail.com>
2026-02-08 14:45:08 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  const nav = document.getElementById("navLinks");
+  nav.style.display = nav.style.display === "flex" ? "none" : "flex";
+}
+function explore() {
+  const btn1 = document.getElementsByClassName("btn1");
+  
+}
